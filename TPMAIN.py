56c43bc6a76e95d34e71121b07f41f854e5d6c1e
GuidoHermann <guidohermanndev@gmail.com>
2025-09-17 15:22:57 -0300

    listado de paises y menu

--- TPMAIN.py
+++ TPMAIN.py
@@ -16,13 +16,33 @@ def cargar_paises (rutacsv):
             paises.append(pais)
         return paises
     
+def mostrar_menu():
+    print(" ===== Menu principal ===== ")
+    print("1. Listar todos los paises.")
+    print("2. Buscar pais por nombre")
+    print("3. Filtrar paises.")
+    print("4. Ordenar Paises.")
+    print("5. Mostrar estadisticas")
+    print("6. Salir.")
+
+def listar_paises(paises):
+    print("=== Listado de Países ===")
+    for pais in paises:1
+        print(f"Nombre: {pais['nombre']}")
+        print(f"Población: {pais['poblacion']:,}")  # separador de miles con comas
+        print(f"Superficie: {pais['superficie']:,} km²")
+        print(f"Continente: {pais['continente']}\n")
+
+
 paises = cargar_paises(rutacsv)
 
-print("Datos cargados")
+while True:
+    mostrar_menu()
+    opcion = input("Seleccione una opcion: ")
 
+    if opcion == "1":
+        listar_paises(paises)
 
-for pais in paises:
-    print(f"Nombre: {pais['nombre']}")
-    print(f"Población: {pais['poblacion']:,} Habitantes") 
-    print(f"Superficie: {pais['superficie']:,} km²")
-    print(f"Continente: {pais['continente']}\n")
+    elif opcion == "2":
+        print()
+
